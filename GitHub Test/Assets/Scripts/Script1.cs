1e89afac456a67a49fe0ed02772251c4fd51cef6
EdvardsGrinitis01 <670266@grimsby.ac.uk>
2026-01-12 14:37:15 +0000

    Kinda a game ngl

--- GitHub Test/Assets/Scripts/Script1.cs
+++ GitHub Test/Assets/Scripts/Script1.cs
@@ -4,9 +4,14 @@ public class NewMonoBehaviourScript : MonoBehaviour
 {
 
     Rigidbody rb;
+    public float LeftSpeed = 2f;
+    public float RightSpeed = 2f;
+    public float BackSpeed = 2f;
+    public float ForwardSpeed = 2f;
+
     public float JumpForce = 5f;
-    public float LeftSpeed = 0.1f;
-    public float RightSpeed = 0.1f;
+    bool IsGrounded;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -16,19 +21,38 @@ public class NewMonoBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetDownDown("d"))
+        if (Input.GetKeyDown("d"))
         {
-            Degub.Log("Right");
+            Debug.Log("Right");
+            rb.AddForce(Vector3.right * RightSpeed, ForceMode.Impulse);
         }
         if (Input.GetKeyDown("a"))
         {
             Debug.Log("Left");
             rb.AddForce(Vector3.left * LeftSpeed, ForceMode.Impulse);
         }
-        if (Input.GetKeyDown("space"))
+        if (Input.GetKeyDown("s"))
+        {
+            Debug.Log("Back");
+            rb.AddForce(Vector3.back * LeftSpeed, ForceMode.Impulse);
+        }
+        if (Input.GetKeyDown("w"))
+        {
+            Debug.Log("Forward");
+            rb.AddForce(Vector3.forward * LeftSpeed, ForceMode.Impulse);
+        }
+
+
+        if (Input.GetKeyDown("space") && IsGrounded)
         {
             Debug.Log("Up");
             rb.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
+            IsGrounded = false;
         }
+    }
+
+    void OnCollisionStay(Collision other) 
+    {
+        IsGrounded = true;
     }
 }
