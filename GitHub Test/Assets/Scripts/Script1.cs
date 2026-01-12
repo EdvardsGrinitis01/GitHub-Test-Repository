0f93c8c448b340c52a37dfc8ca0d18eaa707b0eb
EdvardsGrinitis01 <670266@grimsby.ac.uk>
2026-01-12 12:30:42 +0000

    Script kinda broke

--- GitHub Test/Assets/Scripts/Script1.cs
+++ GitHub Test/Assets/Scripts/Script1.cs
@@ -5,6 +5,8 @@ public class NewMonoBehaviourScript : MonoBehaviour
 
     Rigidbody rb;
     public float JumpForce = 5f;
+    public float LeftSpeed = 0.1f;
+    public float RightSpeed = 0.1f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,14 +16,18 @@ public class NewMonoBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetDownDown("d"))
+        {
+            Degub.Log("Right");
+        }
         if (Input.GetKeyDown("a"))
         {
-            Debug.Log("AAAA");
-            rb.AddForce(-1, 0, 0);
+            Debug.Log("Left");
+            rb.AddForce(Vector3.left * LeftSpeed, ForceMode.Impulse);
         }
         if (Input.GetKeyDown("space"))
         {
-            Debug.Log("Apples");
+            Debug.Log("Up");
             rb.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
         }
     }
