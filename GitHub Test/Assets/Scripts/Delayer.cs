161ebbe595b4319019845a41eb6c8c1bf6295bce
EdvardsGrinitis01 <670266@grimsby.ac.uk>
2026-01-21 12:27:50 +0000

    Stuff

---
+++ GitHub Test/Assets/Scripts/Delayer.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using System.Collections;
+
+public class Delayer : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("h"))
+        {
+            StartCoroutine("Murder");
+        }
+    }
+
+    IEnumerator Murder()
+    {
+        yield return new WaitForSecondsRealtime(2f);
+        Destroy(gameObject);
+
+    }
+
+}
