b7398412aa02256a686778c180e1a60a97826dff
EdvardsGrinitis01 <670266@grimsby.ac.uk>
2026-02-05 11:02:40 +0000

    stuff

--- GitHub Test/Assets/Scripts/CoinScript.cs
+++ GitHub Test/Assets/Scripts/CoinScript.cs
@@ -1,15 +1,16 @@
 using UnityEngine;
 
+
 public class CoinScript : MonoBehaviour
 {
     public int collectedCoins;
     private object statManager;
+    public GameObject Collectable;
+ 
 
     public void ChangeCoins(int amountCollected)
     {
         collectedCoins += amountCollected;
         collectedCoins = Mathf.Clamp(collectedCoins, 0, 99);
     }
-
-
 }
