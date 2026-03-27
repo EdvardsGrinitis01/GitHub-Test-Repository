21aa222f9268d27e4f8370bc3d537860dff44c34
EdvardsGrinitis01 <670266@grimsby.ac.uk>
2026-03-27 14:40:42 +0000

    LAST PUSH
    DONE

--- GitHub Test/Assets/Scripts/SManger.cs
+++ GitHub Test/Assets/Scripts/SManger.cs
@@ -3,8 +3,22 @@ using UnityEngine.SceneManagement;
 
 public class SManger : MonoBehaviour
 {
+    public string SceneName;
     public void scene_changer(string scene_name)
     {
         SceneManager.LoadScene(scene_name);
+    }
+
+    public void Update()
+    {
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                SceneManager.LoadScene(SceneName);
+            }
+
+                
+
+        }
     }
 }
